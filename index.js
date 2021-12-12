4ab43649576cabcf463b80ce08a9ec7a7fe4f473
Daniel Krzeminski <Dankrzeminski32@gmail.com>
2021-12-12 00:20:55 -0600

    got delete item working

--- index.js
+++ index.js
@@ -7,6 +7,12 @@ function createTask() {
   let taskItem = document.createElement("li");
   let taskItemText = document.createTextNode(task);
   taskItem.appendChild(taskItemText);
+  let xIconSpan = document.createElement("span");
+  //let xIcon = document.createTextNode("/u24d8");
+  //xIconSpan.appendChild(xIcon);
+  xIconSpan.innerHTML = "     &#10006";
+  xIconSpan.setAttribute("onclick", "removeItem(this)");
+  taskItem.appendChild(xIconSpan);
   console.log(taskItem);
   addTask(taskItem);
   document.getElementById("task").value = "";
@@ -15,4 +21,11 @@ function createTask() {
 function addTask(task) {
   let taskList = document.getElementById("task-list");
   taskList.appendChild(task);
+}
+
+function removeItem(task) {
+  console.log(task);
+  parentNode = task.parentNode;
+  console.log(parentNode);
+  parentNode.remove();
 }
